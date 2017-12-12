ca87f8aa9d37bc5479c4621d013f2479857b9caa
SiddhiNalam <31481648+SiddhiNalam@users.noreply.github.com>
2017-12-12 16:50:18 +0530

    Update WeatherSMS.py

--- WeatherSMS.py
+++ WeatherSMS.py
@@ -5,8 +5,8 @@ import way2sms
 page = requests.get("https://weather.com/en-IN/weather/today/l/INXX0102:1:IN")
 soup = BeautifulSoup(page.content, 'html.parser')
 today = soup.find(class_="today_nowcard-phrase").get_text()
-username='8237165028'
-password='D4384N'
+username=' '#Enter your credentials
+password=' '
 q=way2sms.sms(username,password)
 q.send( username, today )
 q.logout()
